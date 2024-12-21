70f82573befe350d03e3eb1b003ebec573b9fae7
callum <callum.binns@insitetrack.com>
2024-12-21 21:34:31 +0000

    Confirmed working solution to part 1

--- src/main/java/com/callumhobby/adventofcode2024day3/AdventOfCode2024Day3.java
+++ src/main/java/com/callumhobby/adventofcode2024day3/AdventOfCode2024Day3.java
@@ -4,6 +4,9 @@
 
 package com.callumhobby.adventofcode2024day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author CallumBinns
@@ -12,18 +15,45 @@ public class AdventOfCode2024Day3 {
 
     public static void main(String[] args) {
         InputReader corruptedData = new InputReader("src\\main\\java\\com\\callumhobby\\adventofcode2024day3\\Input.txt");
-        
+        List<Mul> muls = new ArrayList<Mul>();
         for (String ln : corruptedData.getInput()) {
-            
+            String[] possibleMuls = ln.split("mul\\(");
+            for (String possibleMul : possibleMuls) {
+                possibleMul = cleanupHelper(possibleMul);
+                if (possibleMul.length() > 2) {
+                    muls.add(new Mul(possibleMul));
+                }
+  
+            }
         }
+        Integer totalOfMuls = 0;
+        for (Mul mul : muls) {
+            totalOfMuls += mul.product;
+        }
+        System.out.println("Total sum of multiplications: "+totalOfMuls);
     }
     
     
-    public String cleanupHelper(String in){
+    public static String cleanupHelper(String in){
         String outString = "";
+        Character c;
+        for (int i = 0; i < in.length(); i++) {
+            c=in.charAt(i);
+            if (c == ',' || Character.isDigit(c)) {
+                outString += c;
+            }
+            else if(c == ')'){
+                break;
+            }
+            else{
+                return "";
+            }
+        }
+        String[] splitTest = outString.split(",");
+        if (splitTest.length == 2) {
+            return outString;
+        }
+        return "";
         
-        
-        
-        return outString;
     }
 }
