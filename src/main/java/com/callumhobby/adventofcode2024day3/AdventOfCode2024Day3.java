0187fab2fcb8d0a00780fa093725a5feb4e98d8d
callum <callum.binns@insitetrack.com>
2024-12-21 23:29:20 +0000

    confirmed working day 3 solution with frankly silly partial refactor

--- src/main/java/com/callumhobby/adventofcode2024day3/AdventOfCode2024Day3.java
+++ src/main/java/com/callumhobby/adventofcode2024day3/AdventOfCode2024Day3.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.callumhobby.adventofcode2024day3;
 
 import java.util.ArrayList;
@@ -16,36 +15,61 @@ public class AdventOfCode2024Day3 {
     public static void main(String[] args) {
         InputReader corruptedData = new InputReader("src\\main\\java\\com\\callumhobby\\adventofcode2024day3\\Input.txt");
         List<Mul> muls = new ArrayList<Mul>();
-        for (String ln : corruptedData.getInput()) {
-            String[] possibleMuls = ln.split("mul\\(");
-            for (String possibleMul : possibleMuls) {
-                possibleMul = cleanupHelper(possibleMul);
-                if (possibleMul.length() > 2) {
-                    muls.add(new Mul(possibleMul));
-                }
-  
+        corruptedData.customSplit("mul\\(", false);
+        for (String ln : corruptedData.getCustomSplit()) {
+            String cleanString = cleanupHelper(ln);
+            if (cleanString.length() > 2) {
+                muls.add(new Mul(cleanString));
             }
         }
+
         Integer totalOfMuls = 0;
         for (Mul mul : muls) {
             totalOfMuls += mul.product;
         }
-        System.out.println("Total sum of multiplications: "+totalOfMuls);
+        System.out.println("Total sum of multiplications: " + totalOfMuls);
+
+        muls.clear();
+        totalOfMuls = 0;
+        corruptedData.customSplit("don't\\(\\)", true);
+        List<String> donts = corruptedData.getCustomSplit();
+        for (int i = 0; i < donts.size(); i++) {
+            if (donts.get(i).contains("do()")) {
+                muls.addAll(cleanMulLoop(donts.get(i).substring(donts.get(i).indexOf("do()") + 4)));
+            }
+            else if (i == 0){//first one is a do
+                muls.addAll(cleanMulLoop(donts.get(i)));
+            }
+        }
+        
+        for (Mul mul : muls) {
+            totalOfMuls += mul.product;
+        }
+        System.out.println("Total sum of do multiplications: " + totalOfMuls);
+
     }
-    
-    
-    public static String cleanupHelper(String in){
+
+    public static List<Mul> cleanMulLoop(String dataPotentiallyContainingMuls) {
+        List<Mul> muls = new ArrayList<Mul>();
+        for (String possibleMul : dataPotentiallyContainingMuls.split("mul\\(")) {
+            possibleMul = cleanupHelper(possibleMul);
+            if (possibleMul.length() > 2) {
+                muls.add(new Mul(possibleMul));
+            }
+        }
+        return muls;
+    }
+
+    public static String cleanupHelper(String in) {
         String outString = "";
         Character c;
         for (int i = 0; i < in.length(); i++) {
-            c=in.charAt(i);
+            c = in.charAt(i);
             if (c == ',' || Character.isDigit(c)) {
                 outString += c;
-            }
-            else if(c == ')'){
+            } else if (c == ')') {
                 break;
-            }
-            else{
+            } else {
                 return "";
             }
         }
@@ -54,6 +78,7 @@ public class AdventOfCode2024Day3 {
             return outString;
         }
         return "";
-        
+
     }
+
 }
--- src/main/java/com/callumhobby/adventofcode2024day3/InputReader.java
+++ src/main/java/com/callumhobby/adventofcode2024day3/InputReader.java
@@ -9,33 +9,70 @@ import java.util.ArrayList;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 /**
  *
  * @author CallumBinns
  */
 public class InputReader {
+
     private String path;
-    
-    public InputReader(){
+    private List<String> lines;
+    private List<String> customSplit;
+
+    public InputReader() {
         this.path = "Input.txt";
+        this.lines = getInput();
     }
-    
-    public InputReader(String path){
+
+    public InputReader(String path) {
         this.path = path;
+        this.lines = getInput();
     }
-    
-    public List<String> getInput(){
+
+    public List<String> getLines() {
+        return lines;
+    }
+
+    public List<String> getCustomSplit() {
+        return customSplit;
+    }
+
+    private List<String> getInput() {
         List<String> input = new ArrayList<>();
-        try{
+        try {
             File file = new File(path);
             Scanner scan = new Scanner(file);
             while (scan.hasNextLine()) {
-                input.add(scan.nextLine());                
+                input.add(scan.nextLine());
             }
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("input reader has failed with error:" + e.getMessage());
         }
         return input;
     }
+
+    /**
+     *
+     * @param splitter used as regex, escape characters required
+     * @param ignoreLinebreaks for more predictable custom splitting, view as default
+     */
+    public void customSplit(String splitter, boolean ignoreLinebreaks) {
+        List<String> customList = new ArrayList<>();
+        if (ignoreLinebreaks) {
+            String monolithicData = String.join("",lines);
+            String[] splitItems = monolithicData.split(splitter);
+            customList.addAll(Arrays.asList(splitItems));
+
+        } else {
+            for (String ln : lines) {
+                String[] splitItems = ln.split(splitter);
+                customList.addAll(Arrays.asList(splitItems));
+            }
+        }
+
+        this.customSplit = customList;
+    }
+
 }
