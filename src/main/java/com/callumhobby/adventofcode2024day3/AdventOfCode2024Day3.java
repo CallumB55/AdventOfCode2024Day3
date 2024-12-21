9c1a6f674c3a3b865aef7d15c8f674e0ce8ec4f1
callum <callum.binns@insitetrack.com>
2024-12-21 16:35:53 +0000

    initial commit

---
+++ src/main/java/com/callumhobby/adventofcode2024day3/AdventOfCode2024Day3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.callumhobby.adventofcode2024day3;
+
+/**
+ *
+ * @author CallumBinns
+ */
+public class AdventOfCode2024Day3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
